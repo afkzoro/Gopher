b818cc2ccd565655bd8ae20675de7fa6894e4535
Samuel Theophilus <samueltheophilus019@gmail.com>
2024-02-01 06:27:05 +0100

    Update channels, goroutine and mutex in  Go

---
+++ Goroutines/goroutine.go
@@ -0,0 +1,19 @@
+package goroutine
+
+import "fmt"
+
+// simple goroutine
+func goroutine() {
+	ch := make(chan string, 1)
+	go func() {
+		for _, word := range []string{"hello", "world"} {
+			ch <- word
+		}
+		close(ch)
+	}()
+
+	for word := range ch {
+		fmt.Println(word)
+	}
+}
+
---
+++ Mutex/mutex.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type sum struct {
+	mu sync.Mutex
+	sum int
+}
+
+func (s *sum) get() int {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.sum
+}
+
+func (s *sum) add(n int) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.sum += n
+}
+
+func main() {
+	mySum := &sum{}
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func(x int) {
+			defer wg.Done()
+			mySum.add(x)
+		} (i)
+	}
+
+	wg.Wait()
+	fmt.Println("Final sum: ", mySum.get())
+}
