7b94df175108f857e080209e5b9d8983f442b3e5
Samuel Theophilus <samueltheophilus019@gmail.com>
2024-02-05 22:21:29 +0100

    Update context.go

---
+++ Context/context.go
@@ -0,0 +1,30 @@
+package context
+
+import (
+	"time"
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	fmt.Println("Server: hello handler started")
+	defer fmt.Println("Server: hello handler ended")
+
+	select {
+	case <- time.After(10 * time.Second):
+		fmt.Fprintf(w, "hello\n")
+	
+	case <- ctx.Done():
+		err := ctx.Err()
+		fmt.Println("Server:", err)
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+	}
+
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8090", nil)
+}
